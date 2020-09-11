7fd3e020666452b113602a9bb0f937d95875091c
Zhang Cheng <zchelllo@gmail.com>
2020-09-11 13:58:24 +0900

    update for emoji v13.0

--- src/download_from_unicode.sh
+++ src/download_from_unicode.sh
@@ -3,4 +3,4 @@
 mkdir -p data/unicode
 
 wget https://raw.githubusercontent.com/unicode-org/cldr/master/common/annotations/ja.xml -O data/unicode/ja.xml
-wget http://unicode.org/Public/emoji/12.0/emoji-test.txt -O data/unicode/emoji-test.txt
+wget http://unicode.org/Public/emoji/13.0/emoji-test.txt -O data/unicode/emoji-test.txt
